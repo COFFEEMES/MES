dc44c2f00453a514ebdd554e7cc9bf571b5318a1
Jiyeon <gjy30533@naver.com>
2023-04-18 17:43:17 +0900

    0418

--- nocafe/src/main/java/com/yedam/spring/sales/service/MaterialWareService.java
+++ nocafe/src/main/java/com/yedam/spring/sales/service/MaterialWareService.java
@@ -9,4 +9,16 @@ public interface MaterialWareService {
 	
 	//자재발주 상세 목록
 	public List<MaterialWareVO> detailList(String ordrCd);
+	
+	//검사 모달 
+	public List<MaterialWareVO> testPoor();
+	
+	//저장
+	//public int saveRsc(List<MaterialWareVO> materialWareVO);
+	
+	//사원 검색
+	public List<MaterialWareVO> empList();
+	
+	//자재입고 검사 저장
+	public int saveTotal(MaterialWareVO materialwareVO);
 }
--- nocafe/src/main/java/com/yedam/spring/sales/web/MaterialWareController.java
+++ nocafe/src/main/java/com/yedam/spring/sales/web/MaterialWareController.java
@@ -6,18 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.yedam.spring.sales.mapper.MaterialWareMpper;
+import com.yedam.spring.sales.service.MaterialWareService;
 import com.yedam.spring.sales.service.MaterialWareVO;
-import com.yedam.spring.sales.service.OrderVO;
 
 @Controller
 public class MaterialWareController {
 	
 	@Autowired
-	MaterialWareMpper materialWareMapper;
+	MaterialWareService materialWareService;
 
 	//자재입고검사 페이지
 	@GetMapping("/materialReceivingTest")
@@ -29,13 +29,37 @@ public class MaterialWareController {
 	@PostMapping("/reList")
 	@ResponseBody
 	public List<MaterialWareVO> reList(@RequestParam(required = false) String start, @RequestParam(required = false)  String end, @RequestParam(required = false)  String vendNm) {
-			return materialWareMapper.reList(start, end, vendNm);
+			return materialWareService.reList(start, end, vendNm);
 		}
 	
 	//자재 발주 목록
 	@PostMapping("/detailList")
 	@ResponseBody
 	public List<MaterialWareVO> detailList(@RequestParam(required = false) String ordrCd) {
-			return materialWareMapper.detailList(ordrCd);
+			return materialWareService.detailList(ordrCd);
 		}
+	
+	//검사 모달
+	@GetMapping("/testPoor")
+	@ResponseBody
+	public List<MaterialWareVO> testPoor() {
+			return materialWareService.testPoor();
+		}
+	
+		/*
+		 * //저장 -> 쿼리문 짜야함
+		 * 
+		 * @PostMapping("/saveRsc")
+		 * 
+		 * @ResponseBody public int saveRsc(@RequestBody List<MaterialWareVO>
+		 * materialwareVO) { return materialWareService.saveRsc(materialwareVO); }
+		 */
+	
+	//사원정보
+	@GetMapping("/empList")
+	@ResponseBody
+	public List<MaterialWareVO> empList() {
+		return materialWareService.empList();
+	}
+	
 }
